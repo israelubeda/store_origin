5c0e3050d68d1447e33173376dc3e406b705f8d8
Israel Ubeda <36613462+israelubeda@users.noreply.github.com>
2022-10-02 10:50:44 -0300

    Add files via upload

---
+++ app/constants.ts
@@ -0,0 +1,40 @@
+export const INFORMATION = {
+  avatar: "/assets/tulogo.jpg",
+  banner: "/assets/Portada.jpg",
+  title: "Gotarsi titulo",
+  description: "Envianos tu pedido y a la brevedad te respondemos.",
+  description2: "👉 Nuestro horario de atención es de Lunes a Domingo de 10 a 20 hs. Hacemos envíos 🚴‍♀",
+  phone: `56982260964`,
+  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSlD8fPH4XHphHUW2Gtkhp31wCOKk1kLL6kpaOMJEvUq-dsUlnS29aN25rqdJDwFJq20f4hME79rQXj/pub?output=csv`,
+  color: `teal`,
+  social: [
+    {
+      name: "instagram",
+      url: `https://www.instagram.com/gotarsi/`,
+    },
+    {
+      name: "whatsapp",
+      url: `https://wa.me/56982260964`,
+    },
+    {
+      name: "twitter",
+      url: `https://twitter.com/israel_ubeda`,
+    },
+    {
+      name: "youtube",
+      url: `https://www.youtube.com/channel/UCmCioGhTgS2AqkZNNr_WvAA`,
+    },
+    {
+      name: "envelope-o",
+      url: `mailto:israel.ubedabravo@gmail.com`,
+    },
+    {
+      name: "fa-globe",
+      url: `https://israelubeda.github.io/`,
+    },
+    {
+      name: "github",
+      url: `https://github.com/israelubeda`,
+    },
+  ],
+};
